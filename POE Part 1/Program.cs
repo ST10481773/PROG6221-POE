f3c18d47fa30c9418ae0c21be1343bdec5c9c3e0
ST10481773 <ST10481773@rcconnect.edu.za>
2026-03-29 21:02:17 +0200

    Added text-based welcome greeting and logo

--- POE Part 1/Program.cs
+++ POE Part 1/Program.cs
@@ -7,6 +7,12 @@ internal class Program
     {
         SoundPlayer player = new SoundPlayer(Resources.welcome);
         player.Play();
-        
+        Console.Write("                                                                             \r\n                                                                             \r\n▄█████ ▄▄ ▄▄ ▄▄▄▄  ▄▄▄▄▄ ▄▄▄▄   ▄▄▄▄ ▄▄▄▄▄  ▄▄▄▄ ▄▄ ▄▄ ▄▄▄▄  ▄▄ ▄▄▄▄▄▄ ▄▄ ▄▄ \r\n██     ▀███▀ ██▄██ ██▄▄  ██▄█▄ ███▄▄ ██▄▄  ██▀▀▀ ██ ██ ██▄█▄ ██   ██   ▀███▀ \r\n▀█████   █   ██▄█▀ ██▄▄▄ ██ ██ ▄▄██▀ ██▄▄▄ ▀████ ▀███▀ ██ ██ ██   ██     █   \r\n                                                                             ");
+        Console.WriteLine("                                                                               \r\n                                                                               \r\n▄████▄ ▄▄   ▄▄  ▄▄▄  ▄▄▄▄  ▄▄▄▄▄ ▄▄  ▄▄ ▄▄▄▄▄  ▄▄▄▄  ▄▄▄▄   █████▄  ▄▄▄ ▄▄▄▄▄▄ \r\n██▄▄██ ██ ▄ ██ ██▀██ ██▄█▄ ██▄▄  ███▄██ ██▄▄  ███▄▄ ███▄▄   ██▄▄██ ██▀██  ██   \r\n██  ██  ▀█▀█▀  ██▀██ ██ ██ ██▄▄▄ ██ ▀██ ██▄▄▄ ▄▄██▀ ▄▄██▀   ██▄▄█▀ ▀███▀  ██   \r\n                                                                               ");
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello, " + userName + ", and welcome to the Cybersecurity Awareness Bot. Feel free to ask any questions related to cybersecuriy");
+        string userInput = Console.ReadLine();
+
     }
 }
